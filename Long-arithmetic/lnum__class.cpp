03d4aabf8ac42839e2b1c9f21f046bdbeafdad46
Ivan Mikerin <vmik7@yandex.ru>
2020-12-13 02:00:08 +0300

    FFT by Errichto

--- Long-arithmetic/lnum__class.cpp
+++ Long-arithmetic/lnum__class.cpp
@@ -1,3 +1,73 @@
+struct FFT
+{
+    vector<complex<double> > org, res, tmp,e;
+    int n;
+    vector<complex<double> > compute_inverse( vector<complex<double> > x)
+    {
+        // x's length must be a power of 2
+        org=x;
+        n=x.size();
+        res =vector<complex<double > > (n);
+        tmp =vector<complex<double > > (n);
+        e =vector<complex<double > > (n);
+        for (int i=0;i<n;i++)
+            e[i] = exp((2*M_PI*i*complex<double> (0,1))/(double)n);
+        fft(n);
+        for(int i=0;i<n;i++)
+            res[i]/=n;
+        return res;
+    }
+    vector<complex<double> > poly_mult(vector<complex<double> > u, vector<complex < double > > v)
+    {
+        int s = u.size()+v.size();
+        u.resize(s);
+        v.resize(s);
+        while((((int)u.size())&((-(int)u.size()))) != (int)u.size())
+        {
+            u.push_back(0);
+            v.push_back(0);
+        }
+        vector<complex<double> > dftu = compute(u), dftv = compute(v),res;
+        for(int i=0;i<(int)dftu.size();i++)
+            res.push_back(dftu[i]*dftv[i]);
+        return compute_inverse(res);
+    }
+    vector<complex<double> > compute( vector<complex<double> > x)
+    {
+        // x's length must be a power of 2
+        org=x;
+        n=x.size();
+        res =vector<complex<double > > (n);
+        tmp =vector<complex<double > > (n);
+        e =vector<complex<double > > (n);
+        for (int i=0;i<n;i++)
+            e[i] = exp(-(2*M_PI*i*complex<double> (0,1))/(double)n);
+        fft(n);
+        return res;
+    }
+
+    void fft(int l, int p=0,int s=1,int q=0)
+    {
+        if(l==1)
+        {
+            res[q]=org[p];
+            return;
+        }
+        int pt = 0;
+        fft(l/2,p,2*s,q);
+        fft(l/2,p+s,2*s,q+l/2);
+        for(int i=0;i<l/2;i++)
+        {
+            tmp[i] = res[q+i]+e[pt]*res[q+l/2+i];
+            tmp[i+l/2] = res[q+i] - e[pt]*res[q+l/2+i];
+            pt+=s;
+            pt%=n;
+        }
+        for(int i=0;i<l;i++)
+            res[q+i]=tmp[i];
+    }
+};
+
 class lnum {
 private:
     const int Base = 10000000;
@@ -74,39 +144,6 @@ private:
             return false;
         }
     }
-    void fft(vector<complex<long double> > & a, bool invert) const {
-        int n = (int)a.size();
-        for (int i = 1, j = 0; i < n; i++) {
-            int bit = n >> 1;
-
-            while (j >= bit) {
-                j -= bit;
-                bit >>= 1;
-            }
-            j += bit;
-
-            if (i < j)
-                swap(a[i], a[j]);
-        }
-        for (int len = 2; len <= n; len <<= 1) {
-            long double ang = 2 * acos(-1) /len * (invert ? -1 : 1);
-            complex<long double> wlen(cos(ang), sin(ang));
-            for (int i = 0; i < n; i += len) {
-                complex<long double> w(1);
-                for (int j = 0; j < len / 2; j++) {
-                    complex<long double> u = a[i + j],  v = a[i + j + len / 2] * w;
-                    a[i + j] = u + v;
-                    a[i + j + len / 2] = u - v;
-                    w *= wlen;
-                }
-            }
-        }
-        if (invert) {
-            for (int i = 0; i < n; i++)
-                a[i] /= n;
-        }
-        return;
-    }
     vector<int> add(vector<int> a, vector<int> b) const {
         int n = max(a.size(), b.size());
         a.resize(n), b.resize(n);
@@ -125,7 +162,7 @@ private:
         return del_zeros(a);
     }
     vector<int> mul(const vector<int> & a, const vector<int> & b) const {
-        vector<complex<long double> > fa, fb;
+        vector<complex<double> > fa, fb;
         for (auto i : a) fa.pb(i);
         for (auto i : b) fb.pb(i);
 
@@ -135,14 +172,12 @@ private:
         n <<= 1;
         fa.resize(n),  fb.resize(n);
 
-        fft(fa, false),  fft(fb, false);
-        for (int i = 0; i < n; i++)
-            fa[i] *= fb[i];
-        fft(fa, true);
+        FFT FFT_STRUCT;
+        vector<complex<double> > fft_res = FFT_STRUCT.poly_mult(fa, fb);
 
         vector<long long> num(n);
         for (int i = 0; i < n; i++)
-            num[i] = (long long)(fa[i].real() + 0.5);
+            num[i] = (long long)(fft_res[i].real() + 0.5);
 
         long long c = 0;
         for (int i = 0; i < n; i++) {
